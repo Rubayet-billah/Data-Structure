1dcc9d5669d5c551c1a765c2448ed5d812b434b8
Rubayet-billah <rubayetbillah909@gmail.com>
2026-01-11 07:15:35 +0600

    11

---
+++ Random Problems/A_Watermelon.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ll long long
+#define haa cout << "YES\n";
+#define naa cout << "NO\n";
+#define endl cout << "\n";
+#define nl "\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    if (!(n & 1) && n > 2)
+        haa else naa
+}
+
+int main()
+{
+    fast;
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
