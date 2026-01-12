f4c07c00f4a1711d0a3e97ab854a3aa91c9abc7a
Rubayet-billah <rubayetbillah909@gmail.com>
2026-01-13 01:47:46 +0600

    folder restructured

---
+++ D_Children_Holiday.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ll long long
+#define haa cout << "Yes\n";
+#define naa cout << "No\n";
+#define endl cout << "\n";
+#define nl "\n"
+
+void solve()
+{
+    int m, n;
+    cin >> m >> n;
+    vector<tuple<int, int, int>> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        int t, z, y;
+        cin >> t >> z >> y;
+        a[i] = make_tuple(t, z, y);
+    }
+
+    vector<int> can(n);
+    auto ok = [&](int mid)
+    {
+        vector<int> may(n);
+        int has = m;
+        for (int i = 0; i < n; i++)
+        {
+            tuple<int, int, int> T = a[i];
+            int t, z, y, total, can, remaining, curr;
+            t = get<0>(T);
+            z = get<1>(T);
+            y = get<2>(T);
+            total = ((t * z) + y);
+            can = (mid / total);
+            remaining = mid % total;
+            curr = ((can * z) + min(z, (remaining / t)));
+            curr = min(has, curr);
+            has -= curr;
+            may[i] = curr;
+        }
+
+        if (!has)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                can[i] = may[i];
+            }
+        }
+
+        return (!has);
+    };
+
+    int l = 0, r = 1e8, mid, ans;
+    while (l <= r)
+    {
+        mid = l + (r - l) / 2;
+        if (ok(mid))
+        {
+            ans = mid;
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+
+    cout << ans << nl;
+    for (auto val : can)
+    {
+        cout << val << " ";
+    }
+    endl
+}
+
+int main()
+{
+    fast;
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
