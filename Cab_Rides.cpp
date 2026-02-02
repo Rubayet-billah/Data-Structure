0221253ca14640e8cca9e655834f704915e5ca8a
Rubayet-billah <rubayetbillah909@gmail.com>
2026-02-02 16:58:07 +0600

    cop

---
+++ Cab_Rides.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ll long long
+#define haa cout << "Yes\n";
+#define naa cout << "No\n";
+#define endl cout << "\n";
+#define nl "\n"
+
+void solve()
+{
+    int x;
+    cin >> x;
+    cout << max(200, 100 * x) << nl;
+}
+
+int main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
