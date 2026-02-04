63855b30092df3188fb2bcb6405898eab1ca4704
Rubayet-billah <rubayetbillah909@gmail.com>
2026-02-05 02:34:06 +0600

    add

---
+++ C_Rock_and_Lever.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ll long long
+#define haa cout << "Yes\n";
+#define naa cout << "No\n";
+#define endl cout << "\n";
+#define nl "\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        int maxBit = __lg(a[i]);
+        mp[maxBit]++;
+    }
+    ll cnt = 0;
+    for (auto [x, y] : mp)
+    {
+        if (y > 1)
+            cnt += ((1LL * y * (y - 1)) / 2);
+    }
+    cout << cnt << nl;
+}
+
+int main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
