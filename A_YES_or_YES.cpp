690b9a0c8f4c88e2a9fdd6c0614d87a41c002aa1
Rubayet-billah <rubayetbillah909@gmail.com>
2026-04-07 20:37:37 +0600

    st maintain

---
+++ A_YES_or_YES.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ll long long
+#define haa cout << "Yes\n";
+#define naa cout << "No\n";
+#define endl cout << "\n";
+#define nl "\n"
+
+void solve()
+{
+    string s, t;
+    cin >> s;
+    for (char c : s)
+        t += tolower(c);
+
+    if (t == "yes")
+        haa else naa
+}
+
+int main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
