15effc311a038ae280a64ccf597e60f5e503762c
Rubayet-billah <rubayetbillah909@gmail.com>
2026-04-26 23:12:06 +0600

    d

---
+++ A_Plus_or_Minus.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ll long long
+#define haa cout << "Yes\n";
+#define naa cout << "No\n";
+#define endl cout << "\n";
+#define nl "\n"
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a + b == c)
+        cout << "+" nl;
+    else
+        cout << "-" nl;
+}
+
+int main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
---
+++ Multiple_of_3.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ll long long
+#define haa cout << "Yes\n";
+#define naa cout << "No\n";
+#define endl cout << "\n";
+#define nl "\n"
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int fact = n / 3, rem = n % 3;
+    if (rem < 2)
+        cout << fact * 3 << nl;
+    else
+        cout << ((fact + 1) * 3) << nl;
+}
+
+int main()
+{
+    fast;
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
